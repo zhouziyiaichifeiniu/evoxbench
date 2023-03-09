a90578c1e9b28b3bb8abe118db18b9733b80d557
SkyLYnf <skylynf@hotmail.com>
2023-03-09 21:43:59 +0800

    finish airfoil mlp predictor

--- evoxbench/modules/surrogate_model.py
+++ evoxbench/modules/surrogate_model.py
@@ -104,30 +104,105 @@ class MLPPredictor(SurrogateModel):
         return pred
 
 
+class AirFoilMLPPredictor(SurrogateModel):
+    def __init__(self,
+                 pretrained):
+        super().__init__()
+
+        checkpoints = json.load(open(pretrained, 'r'))
+
+        weights = checkpoints['state_dicts']
+        # self.remap_factor = checkpoints['remap_factor']
+        # self.base_acc = checkpoints['base_acc']
+
+        if not isinstance(weights, list):
+            weights = [weights]
+
+        models = []
+        for weight in weights:
+            model = {}
+            for key, values in weight.items():
+                model[key] = np.array(values)
+            # model['normalization_factor'] = weight['normalization_factor']
+            models.append(model)
+
+        self.models = models
+
+    @property
+    def name(self):
+        return 'AirFoil MLP Predictor'
+
+    def fit(self, X, **kwargs):
+        raise NotImplementedError
+
+    def forward(self, X, model):
+        for i in range(1, 3):
+            X = np.matmul(X, model['w{}'.format(i)].transpose()) + model['b{}'.format(i)][None, :]
+            X = np.maximum(X, 0)
+
+        X = np.matmul(X, model['w3'].transpose()) + model['b3'][None, :]
+        X = 1 / (1 + np.exp(-X))
+
+        X = np.matmul(X, model['w4'].transpose())
+        X += model['b4'][None, :]
+        return X
+
+    def predict(self, features, **kwargs):
+        print(f'length of models is {len(self.models)}')
+
+        pred = 0
+        for model in self.models:
+            pred += self.forward(features, model)
+
+        pred = pred / len(self.models)
+
+        return pred
+
+
 if __name__ == '__main__':
     import copy
     import torch
 
-    state_dict = torch.load("/Users/luzhicha/Dropbox/2022/EvoXBench/python_codes/data/mnv3/acc_predictor.pth",
-                            map_location=torch.device("cpu"))
+    # state_dict = torch.load("/Users/luzhicha/Dropbox/2022/EvoXBench/python_codes/data/mnv3/acc_predictor.pth",
+    #                         map_location=torch.device("cpu"))
+    #
+    # for key, values in state_dict.items():
+    #     print(key)
+    #     print(values.size())
+    # exit()
+    # new_state_dict = {}
+    # w_i, b_i = 1, 1
+    # for key, values in state_dict.items():
+    #     if 'weight' in key:
+    #         new_state_dict['W{}'.format(w_i)] = copy.deepcopy(values.cpu().detach().numpy().tolist())
+    #         w_i += 1
+    #     if 'bias' in key:
+    #         new_state_dict['b{}'.format(b_i)] = copy.deepcopy(values.cpu().detach().numpy().tolist())
+    #         b_i += 1
+    #
+    # for key, values in new_state_dict.items():
+    #     print(key)
+    #     print(values)
+    #
+    # with open('/Users/luzhicha/Dropbox/2022/EvoXBench/python_codes/data/mnv3/acc_predictor.json', 'w') as fp:
+    #     json.dump(new_state_dict, fp)
 
-    for key, values in state_dict.items():
-        print(key)
-        print(values.size())
-    exit()
-    new_state_dict = {}
-    w_i, b_i = 1, 1
-    for key, values in state_dict.items():
-        if 'weight' in key:
-            new_state_dict['W{}'.format(w_i)] = copy.deepcopy(values.cpu().detach().numpy().tolist())
-            w_i += 1
-        if 'bias' in key:
-            new_state_dict['b{}'.format(b_i)] = copy.deepcopy(values.cpu().detach().numpy().tolist())
-            b_i += 1
+    with open("C:/Users/admin/Desktop/evoxbench/data/airfoil/501_ctr18.csv", 'r') as f:
+        lines = f.readlines()
 
-    for key, values in new_state_dict.items():
-        print(key)
-        print(values)
+    dataset = []
 
-    with open('/Users/luzhicha/Dropbox/2022/EvoXBench/python_codes/data/mnv3/acc_predictor.json', 'w') as fp:
-        json.dump(new_state_dict, fp)
+    for i in range(1, len(lines)):
+        data = []
+        sp = lines[i].split(",")
+
+        for j in range(1, len(sp)):
+            if j == len(sp) - 1:
+                data.append(sp[j][:-2])
+            else:
+                data.append(sp[j])
+
+        dataset.append(np.array(data, dtype=np.float32))
+
+    mlp = AirFoilMLPPredictor("C:/Users/admin/Desktop/evoxbench/data/airfoil/mlp.json")
+    print(mlp.predict(dataset[0]))
